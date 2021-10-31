16d3689f69850f625fba5678fe212792e0e8128b
Connor Greene <thecatliferules@gmail.com>
2021-10-31 11:32:13 -0400

    New Ion are cool

--- BigChungus/AudioStreamPlayer2D.gd
+++ BigChungus/AudioStreamPlayer2D.gd
@@ -9,6 +9,7 @@ extends AudioStreamPlayer2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	playing = true;
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
